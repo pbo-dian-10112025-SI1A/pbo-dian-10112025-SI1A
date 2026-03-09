2b5e39df994afd0066baa66f648402cb3a17cb67
pbo-dian-10112025-SI1A <diandwinazira@gmail.com>
2026-03-09 09:58:07 +0700

    Add files via upload

---
+++ latihan6.3.php
@@ -0,0 +1,21 @@
+<?php
+
+$data = [
+    ["nama"=>"Ani","nilai"=>80],
+    ["nama"=>"Sinta","nilai"=>75],
+    ["nama"=>"Rina","nilai"=>90]
+];
+
+echo "<table border='1'>";
+echo "<tr><th>Nama</th><th>Nilai</th></tr>";
+
+foreach($data as $d){
+    echo "<tr>";
+    echo "<td>".$d["nama"]."</td>";
+    echo "<td>".$d["nilai"]."</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+
+?>
